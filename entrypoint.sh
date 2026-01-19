3d9a235ac7abf39cb841f42e9a4469739aefc061
EachChen <iach526526@gmail.com>
2026-01-19 22:30:20 +0800

    feat: update environment variables and improve entrypoint script for MinIO integration

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,12 +8,12 @@ echo "Log: MinIO Client installed!"
 # test file download
 mc alias set emfont $MINIO_ENDPOINT $MINIO_USERNAME $MINIO_PASSWORD
 if ${LOCAL_TEST} ; then
-  echo "Log: LOCAL_TEST is true, skip download all font from MinIO."
+  echo "Log: LOCAL_TEST is true, skip download all font from MinIO. Juse download a test file."
   mkdir -p /testing
   # try to connect and download a simple file for testing
-  mc cp emfont/zeabur/css/975HazyGo/200.css /testing
+  mc cp emfont/${MINIO_BUCKET}/css/975HazyGo/200.css /testing
 else
   echo "Log: Downloading fonts from MinIO..."
-    mc mirror --overwrite --remove emfont/zeabur/original-fonts/ /app/src/_data/original-fonts
+    mc mirror --overwrite --remove emfont/${MINIO_BUCKET}/original-fonts/ ${ORIGINAL_FONTS_MOUNTPOINT}
 fi
 exec "$@"
