5084653092ff48a8979dbce830564083562bfcd3
Each Chen <info@iach.cc>
2026-02-14 16:35:24 +0800

    fix: rename language column to languages in font_family table

--- src/_data/sql/schema.sql
+++ src/_data/sql/schema.sql
@@ -36,7 +36,7 @@ CREATE TABLE IF NOT EXISTS font_family (
     tags TEXT[] DEFAULT ARRAY[]::TEXT[], -- 標籤
     repo_url TEXT DEFAULT NULL,
     authors TEXT[] DEFAULT ARRAY[]::TEXT[], -- 作者
-    language JSON, -- 支援語言，language:font_count，語系對字數
+    languages JSON, -- 支援語言，language:font_count，語系對字數
     demo_content_id INT DEFAULT 1,
     format TEXT DEFAULT 'ttf' CHECK (format IN ('otf', 'ttf')), -- 字體格式
     CONSTRAINT valid_category CHECK (category IN ('serif', 'sans-serif', 'monospace', 'cursive', 'fantasy')),
@@ -95,7 +95,7 @@ CREATE TABLE IF NOT EXISTS usage_log (
 -- );
 INSERT INTO font_family (
     id, name, name_zh, name_en, weights, license, version, description,
-    category, family, tags, repo_url, authors, format, language, demo_content_id
+    category, family, tags, repo_url, authors, format, languages, demo_content_id
 ) VALUES (
     'Cubic11',
     '俐方體11號',
